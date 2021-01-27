766d9e65962c9085be0542a4499d6271cc430f35
zack1999 <singhharsh430@gmail.com>
2021-01-27 13:28:45 +0530

    changes

---
+++ chartiq/plugins/timespanevent/studies/projectedVolume.js
@@ -0,0 +1,24 @@
+/**
+ *	8.1.1
+ *	Generation date: 2021-01-27T06:25:04.534Z
+ *	Client name: chartiq-library-trial
+ *	Package Type: Technical Analysis
+ *	License type: annual
+ *	Expiration date: "2021-02-26"
+ *	Domain lock: ["127.0.0.1","localhost","chartiq.com"]
+ *	iFrame lock: true
+ */
+
+/***********************************************************
+ * Copyright by ChartIQ, Inc.
+ * Licensed under the ChartIQ, Inc. Developer License Agreement https://www.chartiq.com/developer-license-agreement
+*************************************************************/
+/*************************************** DO NOT MAKE CHANGES TO THIS LIBRARY FILE!! **************************************/
+/* If you wish to overwrite default functionality, create a separate file with a copy of the methods you are overwriting */
+/* and load that file right after the library has been loaded, but before the chart engine is instantiated.              */
+/* Directly modifying library files will prevent upgrades and the ability for ChartIQ to support your solution.          */
+/*************************************************************************************************************************/
+/* eslint-disable no-extra-parens */
+
+
+// This file is a placeholder and is required.  Do not delete it!
